8316c7cff7937eddd70bf1803110d28ed7f4e7a3
Vishal Nivangune <95202004+VishalAnna@users.noreply.github.com>
2023-06-16 23:10:56 +0530

    Update 21.Immediate Food Delivery II.sql

--- 21.Immediate Food Delivery II.sql
+++ 21.Immediate Food Delivery II.sql
@@ -1,5 +1,7 @@
 https://leetcode.com/problems/immediate-food-delivery-ii/?envType=study-plan-v2&envId=top-sql-50
 
+
+
 # Write your MySQL query statement below
 select round(sum(if(order_date = customer_pref_delivery_date, 1, 0)) / count(*) * 100, 2) as immediate_percentage 
 from Delivery
